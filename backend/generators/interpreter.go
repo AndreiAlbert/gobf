31fb21492ef0d7a17f3c08bf8ba8fa673c99a3b2
andrei <andrei@192-168-0-174.rdsnet.ro>
2023-08-30 15:16:34 -0400

    added support for loops

--- backend/generators/interpreter.go
+++ backend/generators/interpreter.go
@@ -9,9 +9,10 @@ import (
 )
 
 type Interpreter struct {
-	tokens      []lexer.Token
-	memory      [3000]int
-	currPointer int
+	tokens        []lexer.Token
+	memory        [3000]int
+	memoryPointer int
+	tokenPointer  int
 }
 
 func New(input string) *Interpreter {
@@ -22,26 +23,53 @@ func New(input string) *Interpreter {
 
 func (i *Interpreter) Evaluate() strings.Builder {
 	var str strings.Builder
-	for _, token := range i.tokens {
+	loopStack := []int{}
+	for i.tokenPointer < len(i.tokens) {
+		token := i.tokens[i.tokenPointer]
 		switch token.LiteralValue {
 		case lexer.INC_VALUE:
-			i.memory[i.currPointer]++
+			i.memory[i.memoryPointer]++
 		case lexer.DEC_VALUE:
-			i.memory[i.currPointer]--
+			i.memory[i.memoryPointer]--
 		case lexer.INC_POINTER:
-			i.currPointer++
+			i.memoryPointer++
 		case lexer.DEC_POINTER:
-			i.currPointer--
+			i.memoryPointer--
 		case lexer.OUTPUT:
-			str.WriteString(string(rune(i.memory[i.currPointer])))
+			str.WriteString(string(rune(i.memory[i.memoryPointer])))
 		case lexer.INPUT:
 			buf := make([]byte, 1)
 			_, err := os.Stdin.Read(buf)
 			if err != nil {
 				log.Fatal("Could not read standard input")
 			}
-			i.memory[i.currPointer] = int(buf[0])
+			i.memory[i.memoryPointer] = int(buf[0])
+		case lexer.LOOP_START:
+			if i.memory[i.memoryPointer] == 0 {
+				loopDepth := 1
+				for idx := i.tokenPointer + 1; idx < len(i.tokens); idx++ {
+					loopToken := i.tokens[idx]
+					if loopToken.Type == lexer.LOOP_START {
+						loopDepth++
+					} else if loopToken.Type == lexer.LOOP_END {
+						loopDepth--
+						if loopDepth == 0 {
+							i.tokenPointer = idx + 1
+							break
+						}
+					}
+				}
+			} else {
+				loopStack = append(loopStack, i.tokenPointer)
+			}
+		case lexer.LOOP_END:
+			if i.memory[i.memoryPointer] != 0 {
+				i.tokenPointer = loopStack[len(loopStack)-1]
+			} else {
+				loopStack = loopStack[:len(loopStack)-1]
+			}
 		}
+		i.tokenPointer++
 	}
 	return str
 }
